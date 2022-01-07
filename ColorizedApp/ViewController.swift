29bcb585b9879a067dd62c0a34bf2cb38a84bbf3
fabet <asilyk112@gmail.com>
2022-01-07 04:55:52 +0300

    Fix some logical issues

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
     //MARK: - IB Outlets
     @IBOutlet private var colorView: UIView!
 
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+
     @IBOutlet private var redSliderValue: UILabel!
     @IBOutlet private var greenSliderValue: UILabel!
     @IBOutlet private var blueSliderValue: UILabel!
@@ -22,6 +26,7 @@ class ViewController: UIViewController {
         colorView.layer.cornerRadius = 15
         colorView.layer.borderWidth = 5
         colorView.layer.borderColor = UIColor.black.cgColor
+
         changeColor()
     }
 
@@ -43,16 +48,14 @@ class ViewController: UIViewController {
 
     //MARK: - Private Methods
     private func changeValue(of value: UILabel, from sender: UISlider) {
-        let sliderValue = round(sender.value * 100) / 100
-        value.text = String(sliderValue)
+        value.text = String(format: "%.2f", sender.value)
     }
 
     private func changeColor() {
-        let red = CGFloat(Double(redSliderValue.text!)!)
-        let green = CGFloat(Double(greenSliderValue.text!)!)
-        let blue = CGFloat(Double(blueSliderValue.text!)!)
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
 
-        let newColor = CGColor(red: red, green: green, blue: blue, alpha: 1)
-        colorView.layer.backgroundColor = newColor
+        colorView.layer.backgroundColor = CGColor(red: red, green: green, blue: blue, alpha: 1)
     }
 }
